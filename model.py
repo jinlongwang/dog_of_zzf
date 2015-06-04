f701c4f1b2800ccfbb045cc4a7149134dad8349b
jinlong <wangjl@vilstart.com>
2015-06-04 21:33:41 +0800

    model
    peewee model

---
+++ model.py
@@ -0,0 +1,34 @@
+# -*- coding=utf-8 -*-
+"""
+this is peewee model
+"""
+__author__ = "jinlong"
+from peewee import *
+db = MySQLDatabase("zzf", user="root", passwd="")
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class News(BaseModel):
+    title = CharField()
+    content = CharField(null=True)
+    date = DateField(null=True)
+    link = CharField(null=True)
+    type = IntegerField(null=True)
+    update_time = DateTimeField(null=True)
+
+#News.create_table()
+if __name__ == "__main__":
+    #News.create_table()
+    a = News(title="hehe")
+    a.save()
+
+
+
+
+
+
+
+
