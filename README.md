c9879779a59320d1fb8ac397f7734ef89bc11c62
jinlong <jinlongwang89@gmail.com>
2015-06-09 12:03:57 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dog_of_zzf
-to catch the news of zizufang and notify ne
+to catch the news of zizufang and notify by email
+
+#news form
+[建委网站地址](http://www.bjjs.gov.cn/publish/portal0/tab4021/?COLLCC=1160220941&)
+
+#usage
+```python
+ python req.py
+```
--- req.py
+++ req.py
@@ -1,6 +1,8 @@
 # -*- coding=utf-8 -*-
+import datetime
 import requests
 from bs4 import *
+from mailService import *
 
 #ess_ctr10041_ListC_Info_LstC_Info
 #
@@ -8,21 +10,44 @@ def get_content_from_url(url):
     res = requests.get(url)
     if res.status_code == 200:
         content = res.content
-        #print content
         bsContent = BeautifulSoup(content)
         news = bsContent.find(attrs={'id': "ess_ctr10041_ListC_Info_LstC_Info"})
         infos = news.find_all("table")
+        msg = []
         for info in infos:
             detail = info.find_all("td")
             title = detail[1].find("a").attrs.get("title")
             href = detail[1].find("a").attrs.get("href")
             time = detail[2].text
             print title, href, time
+            tempInfo = {
+                'title': title,
+                'href': href,
+                'time': time
+            }
+            msg.append(tempInfo)
+        return msg
+
 
 def analysis(*args, **kwargs):
     pass
 
+def timer(func, param):
+    while True:
+        try:
+            minute = datetime.datetime.now().second
+            if minute == 0:
+                result = func(param)
+                for res in result:
+                    title = res.get('title')
+                    if title.find("中海") > 0:
+                        content = "详情请点击:" + res.get('href') + "<br>"
+                        content += res.get("time")
+                        send_message_mandrillcc(['superrz@163.com', 'yulu0511@126.com'], title, content)
+        except Exception,e:
+            print e
+            continue
 
-get_content_from_url("http://www.bjjs.gov.cn/publish/portal0/tab4021/")
-
+if __name__ == "__main__":
+    timer(analysis, 'http://www.bjjs.gov.cn/publish/portal0/tab4021/')
 
